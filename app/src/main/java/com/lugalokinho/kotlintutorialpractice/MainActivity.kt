9ec2cb7e3ef324f9f20b364dc40bba8f46c2145b
Luiz Miguel <luizjarduli@gmail.com>
2026-01-02 10:56:42 -0300

    feat: (bonus) added navigation bar to switch back and fourth between the practices

--- app/src/main/java/com/lugalokinho/kotlintutorialpractice/MainActivity.kt
+++ app/src/main/java/com/lugalokinho/kotlintutorialpractice/MainActivity.kt
@@ -12,12 +12,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -25,6 +38,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.lugalokinho.kotlintutorialpractice.ui.theme.KotlinTutorialPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +51,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             KotlinTutorialPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Quadrant()
+                    AppWithNavigationBar()
                 }
             }
         }
@@ -191,10 +208,80 @@ fun Quadrant(modifier: Modifier = Modifier) {
 
 // [END quadrant_practice]
 
+// [START navigation_host]
+
+enum class Destination(
+    val route: String,
+    val label: String,
+    val icon: ImageVector,
+    val contentDescription: String
+) {
+    TUTORIAL("tutorial", "Tutorial", Icons.Default.Info, "Tutorial"),
+    COMPLETED_TASK("completed_task", "Completed Task", Icons.Default.CheckCircle, "Completed Task"),
+    QUADRANT("quadrant", "Quadrant", Icons.Default.AccountBox, "Quadrant")
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController,
+        startDestination = startDestination.route
+    ) {
+        Destination.entries.forEach { destination ->
+            composable(destination.route) {
+                when (destination) {
+                    Destination.TUTORIAL -> Tutorial()
+                    Destination.COMPLETED_TASK -> CompletedTask()
+                    Destination.QUADRANT -> Quadrant()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppWithNavigationBar(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val startDestination = Destination.TUTORIAL
+    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                Destination.entries.forEachIndexed { index, destination ->
+                    NavigationBarItem(
+                        selected = selectedDestination == index,
+                        onClick = {
+                            navController.navigate(route = destination.route)
+                            selectedDestination = index
+                        },
+                        icon = {
+                            Icon(
+                                destination.icon,
+                                contentDescription = destination.contentDescription
+                            )
+                        },
+                        label = { Text(destination.label) }
+                    )
+                }
+            }
+        }
+    ) { contentPadding ->
+        AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding))
+    }
+}
+
+// [END navigation_host]
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KotlinTutorialPracticeTheme {
-        Quadrant()
+        AppWithNavigationBar()
     }
 }
